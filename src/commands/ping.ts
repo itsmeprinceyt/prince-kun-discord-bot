3b44c6975300172f8c8f94ca05dd86be8d758553
itsmeprinceyt <114930668+itsmeprinceyt@users.noreply.github.com>
2025-02-03 02:50:55 +0530

    added admin only stuff to /ping to test

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -4,9 +4,27 @@ import { Command } from '../types/Command';
 const pingCommand: Command = {
   data: new SlashCommandBuilder()
     .setName('ping')
-    .setDescription('Replies with Pong!'),
+    .setDescription('Replies with something Mambo!!'),
   async execute(interaction: ChatInputCommandInteraction) {
-    await interaction.reply('Pong!');
+    const ownerId = interaction.guild?.ownerId;
+
+    if (!interaction.guild) {
+      await interaction.reply({
+        content: '😂 Use it in a server, you idiot!',
+        flags: 64,
+      });
+      return;
+    }
+
+    if (interaction.user.id !== ownerId) {
+      await interaction.reply({
+        content: '🚫 You must be the server owner to use this command!',
+        flags: 64,
+      });
+      return;
+    }
+
+    await interaction.reply('https://media.tenor.com/vn3L0I7IjR4AAAAM/uma-uma-musume.gif');
   },
 };
 
