abf9615f0ceef148fa4eb12e0c3383cc709b7f01
itsmeprinceyt <114930668+itsmeprinceyt@users.noreply.github.com>
2025-02-08 19:29:35 +0530

    removed debugging command lines

--- copyAssets.ts
+++ copyAssets.ts
@@ -4,9 +4,6 @@ import { resolve } from "path";
 const srcPath = resolve("src/public");
 const destPath = resolve("dist/src/public");
 
-console.log(`[ DEBUG ] Source Path: ${srcPath}`);
-console.log(`[ DEBUG ] Destination Path: ${destPath}`);
-
 if (!existsSync(srcPath)) {
     console.error("[ ERROR ] Source folder does not exist!");
     process.exit(1);
--- dist/copyAssets.js
+++ dist/copyAssets.js
@@ -4,8 +4,6 @@ const fs_extra_1 = require("fs-extra");
 const path_1 = require("path");
 const srcPath = (0, path_1.resolve)("src/public");
 const destPath = (0, path_1.resolve)("dist/src/public");
-console.log(`[ DEBUG ] Source Path: ${srcPath}`);
-console.log(`[ DEBUG ] Destination Path: ${destPath}`);
 if (!(0, fs_extra_1.existsSync)(srcPath)) {
     console.error("[ ERROR ] Source folder does not exist!");
     process.exit(1);
